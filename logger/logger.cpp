8ead87bd176637093eb39c49618a35302ff0e926
zengchao <zengchao@navercorp.com>
2018-11-08 15:45:48 +0900

    use template

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -12,13 +12,13 @@ const uint64_t log_file_size_limit = 100 * 1024 * 1024; // 100M
 
 void init_logger()
 {
-    LogManager::instance()->init("./", "log", log_file_size_limit);
-    LogManager::instance()->set_log_level(INFO);
+    LogManager<>::instance()->init("./", "log", log_file_size_limit);
+    LogManager<>::instance()->set_log_level(INFO);
 }
 
 void set_log_level(SeverityLevel level)
 {
-    LogManager::instance()->set_log_level(level);
+    LogManager<>::instance()->set_log_level(level);
 }
 
 void write_log(SeverityLevel level, const char* format, ...)
@@ -34,6 +34,6 @@ void write_log(SeverityLevel level, const char* format, ...)
     record.tid = syscall(SYS_gettid);
     record.message = log_message;
 
-    LogManager::instance()->write_log(record);
+    LogManager<>::instance()->write_log(record);
 }
 
--- logger/src/core/log_manager.h
+++ logger/src/core/log_manager.h
@@ -14,22 +14,85 @@
 namespace logger
 {
 
+template<typename Writer = FileWriter,
+    typename Filter = LevelFilter,
+    typename Formmater = DefaultFormmater>
 class LogManager
 {
 public:
-    static LogManager* instance();
+    static LogManager* instance()
+    {
+        if (NULL == m_instance)
+        {
+            m_instance = new LogManager;
+        }
 
-    bool init(const std::string& path, const std::string& filename, uint64_t filesize);
+        return m_instance;
+    }
+
+    bool init(const std::string& path, const std::string& filename, uint64_t filesize)
+    {
+        m_writer.set_file_path(path);
+		m_writer.set_file_name(filename);
+		m_writer.set_file_size(filesize);
+
+		pthread_attr_t  attr;
+		if (0 != pthread_attr_init(&attr))
+		{
+			return false;
+		}
+
+		if (0 != pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED))
+		{
+			pthread_attr_destroy(&attr);
+			return false;
+		}
+
+		if (0 != pthread_create(&m_tid, &attr, log_thread, this))
+		{
+			pthread_attr_destroy(&attr);
+			return false;
+		}
+
+		pthread_attr_destroy(&attr);
+
+		return true;
+    }
 
     void set_log_level(SeverityLevel log_level)
     {
         m_log_level = log_level;
     }
 
-    void write_log(const Record& record);
+    void write_log(const Record& record)
+    {
+        if (!m_filter.filter(record.level, m_log_level))
+		{
+			return;
+		}
+
+		m_log_queue.push(record);
+    }
 
 private:
-    static void* log_thread(void* arg);
+    static void* log_thread(void* arg)
+    {
+        if (NULL == arg)
+		{
+			return NULL;
+		}
+
+		LogManager* p_this = (LogManager*)arg;
+
+		while (true)
+		{
+			Record log_record = p_this->m_log_queue.pop();
+			std::string log_string = p_this->m_formatter.format(log_record);
+			p_this->m_writer.write_log(log_string);
+		}
+
+		return NULL;
+    }
 
 private:
     LogManager() : m_tid(0), m_log_level(INFO)
@@ -47,10 +110,16 @@ private:
     SeverityLevel m_log_level;
 
     ConditionQueue<Record> m_log_queue;    
-    FileWriter m_writer;
-    LevelFilter m_filter;
-    DefaultFormmater m_formatter;
+    Writer m_writer;
+    Filter m_filter;
+    Formmater m_formatter;
 };
+
+
+template<typename Writer,
+    typename Filter,
+    typename Formmater>
+LogManager<Writer, Filter, Formmater>* LogManager<Writer, Filter, Formmater>::m_instance = NULL;
 
 } // namespace logger
 
